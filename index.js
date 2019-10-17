71a3d41cc77b876040f8a053ea815e571922812f
Claret Nnamocha <devclareo@gmail.com>
2019-10-17 22:26:59 +0100

    adding test directory

--- index.js
+++ index.js
@@ -1,20 +1,3 @@
 const { AlphaORM  } = require('./src/alpha-orm')
 
-AlphaORM.setup('mysql',{
-  host     : 'localhost',
-  user     : 'root',
-  password : '',
-  database : 'hng'
-})
-
-AlphaORM.find('records','id = :id ',{id : "soma"}).then((record)=>{
-	console.log(record)
-})
-
-// AlphaORM.create('records').then(async (record)=>{
-// 	record.firstname = "claret"
-// 	record.lastname = "soma"
-// 	record.age = 10
-// 	record.somaage = 30
-// 	AlphaORM.store(record)
-// })
+module.exports { AlphaORM }
---
+++ tests/test.js
@@ -0,0 +1,20 @@
+const { AlphaORM  } = require('../src/alpha-orm')
+
+AlphaORM.setup('mysql',{
+  host     : 'localhost',
+  user     : 'root',
+  password : '',
+  database : 'hng'
+})
+
+AlphaORM.find('records','id = :id ',{id : "soma"}).then((record)=>{
+	console.log(record)
+})
+
+// AlphaORM.create('records').then(async (record)=>{
+// 	record.firstname = "claret"
+// 	record.lastname = "soma"
+// 	record.age = 10
+// 	record.somaage = 30
+// 	AlphaORM.store(record)
+// })
