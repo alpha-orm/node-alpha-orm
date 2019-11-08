70d1f6771565f7f9d82859d7944877b67de6f510
Claret Nnamocha <devclareo@gmail.com>
2019-11-08 17:28:04 +0100

    Create test.js

---
+++ tests/test.js
@@ -0,0 +1,80 @@
+const { AlphaORM } = require('../index')
+
+/**
+ * creating
+ */
+async function creating() {
+    product = await AlphaORM.create('shop_product')
+    product.name = "Running Shoes"
+    product.price = 1000
+    product.stock = 50
+    await AlphaORM.store(product)
+}
+
+/**
+ * creating [foreign key]
+ */
+async function creating_2() {
+    user = await AlphaORM.create('user')
+    user.firstname = "Claret"
+    user.lastname = "Nnamocha"
+    user.age = 21
+    user.birthday = '8-October-1998'
+
+    student = await AlphaORM.create('student')
+    student.matno = "15/31525"
+    student.user = user
+
+    await AlphaORM.store(student)
+    student.happy = true
+    await AlphaORM.store(student)
+}
+
+/**
+ * reading [one] (filter)
+ */
+async function reading() {
+    product = await AlphaORM.find('shop_product', 'id = :id', { id: 3 })
+    console.log(product)
+}
+
+
+/**
+ * reading [all]
+ */
+async function reading_2() {
+    products = await AlphaORM.getAll('shop_product')
+    console.log(products)
+}
+
+/**
+ * reading [all] (filter)
+ */
+async function reading_3() {
+    products = await AlphaORM.findAll('student', 'id > 0')
+    console.log(products)
+}
+
+/**
+ * update
+ */
+async function update() {
+    product = await AlphaORM.find('shop_product', 'id = :id', { id: 1 })
+    product.price = 500
+    await AlphaORM.store(product)
+    product.price = 5000
+    await AlphaORM.store(product)
+    console.log(product)
+}
+
+
+/**
+ * delete
+ */
+async function del() {
+    product = await AlphaORM.find('shop_product', 'id = :id', { id: 1 })
+    await AlphaORM.drop(product)
+}
+
+
+module.exports = { creating, creating_2, reading, reading_2, reading_3, update, del }
